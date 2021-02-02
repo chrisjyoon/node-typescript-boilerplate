1d233e37411a504e57041696da9aa18018bafe58
chrisyoon <chrsjyoon@gmail.com>
2021-02-02 19:58:10 +1100

    apply donenv

--- controllers/home.ts
+++ controllers/home.ts
@@ -3,6 +3,7 @@ import express from 'express';
 const router = express.Router();
 
 router.get('/', (req, res) => {
+  console.log(process.env.PORT);
   res.send('Hello Express + Typescript');
 });
 
--- index.ts
+++ index.ts
@@ -1,11 +1,13 @@
 import express from 'express';
 import morgan from 'morgan';
+
+import config from './config/config';
 // controllers
 import home from './controllers/home';
 import email from './controllers/email';
 
 const app = express();
-const PORT = 8000;
+const PORT = config.port;
 
 app.use(morgan('dev'));
 
